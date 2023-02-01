60c52036024e44b1e24f8bc8b955d693a3cb68d3
Sebastian Zambrano Arango <sebastianarango201316@hotmail.com>
2023-02-01 12:00:05 +0100

    Initial Commit

--- src/defi-contribute.ts
+++ src/defi-contribute.ts
@@ -1,98 +1,250 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
 import {
-  DefiContribute,
-  AdminRevoke,
+  NewAdmin as NewAdminEvent,
+  AdminRevoke as AdminRevokeEvent,
+  ApprovedDonee as ApprovedDoneeEvent,
+  NewDoneeRequest as NewDoneeRequestEvent,
+  DoneeRequestCanceled as DoneeRequestCanceledEvent,
+  NewDonee as NewDoneeEvent,
+  RejectedDonee as RejectedDoneeEvent,
+  Donation as DonationEvent,
+  RedFlag as RedFlagEvent,
+  ReadyToRemove as ReadyToRemoveEvent,
+  EliminatedDonee as EliminatedDoneeEvent,
+  Withdrawal as WithdrawalEvent,
+  SummedToThePool as SummedToThePoolEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+} from "../generated/DefiContribute/DefiContribute";
+import {
+  ActiveAdmin,
+  DeletedAdmin,
+  ActiveDoneeRequest,
   ApprovedDonee,
+  ActiveDonee,
+  DeletedDonee,
   Donation,
-  DoneeRequestCanceled,
-  EliminatedDonee,
-  NewAdmin,
-  NewDonee,
-  NewDoneeRequest,
-  OwnershipTransferred,
-  ReadyToRemove,
   RedFlag,
-  RejectedDonee,
+  ReadyToRemove,
   SummedToThePool,
-  Withdrawal
-} from "../generated/DefiContribute/DefiContribute"
-import { ExampleEntity } from "../generated/schema"
+  Withdrawal,
+  Owner,
+} from "../generated/schema";
 
-export function handleAdminRevoke(event: AdminRevoke): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
+let deadAddress = "0x000000000000000000000000000000000000dEaD";
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
+export function handleNewAdmin(event: NewAdminEvent): void {
+  let activeAdmin = ActiveAdmin.load(event.params.admin.toHexString());
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  if (!activeAdmin) {
+    activeAdmin = new ActiveAdmin(event.params.admin.toHexString());
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  activeAdmin.admin = event.params.admin;
 
-  // Entity fields can be set based on event parameters
-  entity.admin = event.params.admin
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.addressToDoneeId(...)
-  // - contract.addressToRequestsSent(...)
-  // - contract.admin(...)
-  // - contract.doneeCandidateToFreezeTime(...)
-  // - contract.doneePetition(...)
-  // - contract.doneeToStatus(...)
-  // - contract.doneeToWithdrawalFreeze(...)
-  // - contract.doneesId(...)
-  // - contract.idToDonee(...)
-  // - contract.idToRedFlags(...)
-  // - contract.owner(...)
-  // - contract.randomDonationPool(...)
-  // - contract.toNewDonee(...)
+  activeAdmin.save();
 }
 
-export function handleApprovedDonee(event: ApprovedDonee): void {}
+export function handleAdminRevoke(event: AdminRevokeEvent): void {
+  let deletedAdmin = DeletedAdmin.load(event.params.admin.toHexString());
 
-export function handleDonation(event: Donation): void {}
+  if (!deletedAdmin) {
+    deletedAdmin = new DeletedAdmin(event.params.admin.toHexString());
+  }
 
-export function handleDoneeRequestCanceled(event: DoneeRequestCanceled): void {}
+  let activeAdmin = ActiveAdmin.load(event.params.admin.toHexString());
 
-export function handleEliminatedDonee(event: EliminatedDonee): void {}
+  deletedAdmin.admin = event.params.admin;
+  activeAdmin!.admin = Address.fromString(deadAddress);
 
-export function handleNewAdmin(event: NewAdmin): void {}
+  deletedAdmin.save();
+  activeAdmin!.save();
+}
 
-export function handleNewDonee(event: NewDonee): void {}
+export function handleApprovedDonee(event: ApprovedDoneeEvent): void {
+  let approvedDonee = ApprovedDonee.load(event.params.donee.toHexString());
+  let activeDoneeRequest = ActiveDoneeRequest.load(
+    event.params.donee.toHexString()
+  );
 
-export function handleNewDoneeRequest(event: NewDoneeRequest): void {}
+  if (!approvedDonee) {
+    approvedDonee = new ApprovedDonee(event.params.donee.toHexString());
+  }
 
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
+  approvedDonee.donee = event.params.donee;
+  activeDoneeRequest!.donee = Address.fromString(deadAddress);
+  activeDoneeRequest!.message = "";
 
-export function handleReadyToRemove(event: ReadyToRemove): void {}
+  approvedDonee.save();
+  activeDoneeRequest!.save();
+}
 
-export function handleRedFlag(event: RedFlag): void {}
+export function handleNewDoneeRequest(event: NewDoneeRequestEvent): void {
+  let activeDoneeRequest = ActiveDoneeRequest.load(
+    event.params.donee.toHexString()
+  );
 
-export function handleRejectedDonee(event: RejectedDonee): void {}
+  if (!activeDoneeRequest) {
+    activeDoneeRequest = new ActiveDoneeRequest(
+      event.params.donee.toHexString()
+    );
+  }
 
-export function handleSummedToThePool(event: SummedToThePool): void {}
+  activeDoneeRequest.donee = event.params.donee;
+  activeDoneeRequest.message = event.params.message;
 
-export function handleWithdrawal(event: Withdrawal): void {}
+  activeDoneeRequest.save();
+}
+
+export function handleDoneeRequestCanceled(
+  event: DoneeRequestCanceledEvent
+): void {
+  let activeDoneeRequest = ActiveDoneeRequest.load(
+    event.params.donee.toHexString()
+  );
+
+  activeDoneeRequest!.donee = Address.fromString(deadAddress);
+  activeDoneeRequest!.message = "";
+
+  activeDoneeRequest!.save();
+}
+
+export function handleNewDonee(event: NewDoneeEvent): void {
+  let activeDonee = ActiveDonee.load(event.params.doneeId.toHexString());
+  let activeDoneeRequest = ActiveDoneeRequest.load(
+    event.params.donee.toHexString()
+  );
+
+  if (!activeDonee) {
+    activeDonee = new ActiveDonee(event.params.doneeId.toHexString());
+  }
+
+  activeDonee.cause = event.params.cause;
+  activeDonee.donee = event.params.donee;
+  activeDonee.doneeId = event.params.doneeId;
+  activeDonee.message = event.params.message;
+
+  activeDoneeRequest!.donee = Bytes.fromHexString(deadAddress);
+  activeDoneeRequest!.message = "";
+
+  activeDonee.save();
+  activeDoneeRequest!.save();
+}
+
+export function handleRejectedDonee(event: RejectedDoneeEvent): void {
+  let activeDoneeRequest = ActiveDoneeRequest.load(
+    event.params.donee.toHexString()
+  );
+
+  activeDoneeRequest!.donee = Address.fromString(deadAddress);
+
+  activeDoneeRequest!.save();
+}
+
+export function handleDonation(event: DonationEvent): void {
+  let donation = new Donation(
+    event.params.from.toHexString() + event.block.number.toHexString()
+  );
+
+  donation.to = event.params.to;
+  donation.from = event.params.from;
+  donation.amount = event.params.amount;
+
+  donation.save();
+}
+
+export function handleRedFlag(event: RedFlagEvent): void {
+  let redFlag = RedFlag.load(event.params.doneeId.toHexString());
+
+  if (!redFlag) {
+    redFlag = new RedFlag(event.params.doneeId.toHexString());
+  }
+
+  redFlag.cause = event.params.cause;
+  redFlag.doneeId = event.params.doneeId;
+  redFlag.donee = event.params.donee;
+  redFlag.admin = event.params.admin;
+
+  redFlag.save();
+}
+
+export function handleReadyToRemove(event: ReadyToRemoveEvent): void {
+  let readyToRemove = ReadyToRemove.load(event.params.id.toHexString());
+
+  if (!readyToRemove) {
+    readyToRemove = new ReadyToRemove(event.params.id.toHexString());
+  }
+
+  readyToRemove.cause = event.params.cause;
+  readyToRemove.doneeId = event.params.id;
+  readyToRemove.donee = event.params.wallet;
+
+  readyToRemove.save();
+}
+
+export function handleEliminatedDonee(event: EliminatedDoneeEvent): void {
+  let deletedDonee = DeletedDonee.load(event.params.doneeId.toHexString());
+  let activeDonee = ActiveDonee.load(event.params.doneeId.toHexString());
+
+  if (!deletedDonee) {
+    deletedDonee = new DeletedDonee(event.params.doneeId.toHexString());
+  }
+
+  deletedDonee.cause = event.params.cause;
+  deletedDonee.doneeId = event.params.doneeId;
+  deletedDonee.donee = event.params.donee;
+
+  activeDonee!.cause = "";
+  activeDonee!.donee = Address.fromString(deadAddress);
+  activeDonee!.doneeId = BigInt.fromI32(0);
+  activeDonee!.message = "";
+
+  deletedDonee.save();
+  activeDonee!.save();
+}
+
+export function handleWithdrawal(event: WithdrawalEvent): void {
+  let withdrawal = Withdrawal.load(
+    event.params.doneeId.toHexString() + event.block.number.toHexString()
+  );
+
+  if (!withdrawal) {
+    withdrawal = new Withdrawal(
+      event.params.doneeId.toHexString() + event.block.number.toHexString()
+    );
+  }
+
+  withdrawal.doneeId = event.params.doneeId;
+  withdrawal.donee = event.params.donee;
+  withdrawal.proceeds = event.params.proceeds;
+
+  withdrawal.save();
+}
+
+export function handleSummedToThePool(event: SummedToThePoolEvent): void {
+  let summedToThePool = SummedToThePool.load(
+    event.params.amount.toHexString() + event.block.number.toHexString()
+  );
+
+  if (!summedToThePool) {
+    summedToThePool = new SummedToThePool(
+      event.params.amount.toHexString() + event.block.number.toHexString()
+    );
+  }
+
+  summedToThePool.amount = event.params.amount;
+
+  summedToThePool.save();
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  // Index the new owner of the contract
+  let owner = Owner.load(event.params.newOwner.toHexString());
+  if (!owner) {
+    owner = new Owner(event.params.newOwner.toHexString());
+  }
+  owner.owner = event.params.newOwner;
+
+  owner.save();
+}
